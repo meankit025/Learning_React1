1b7d67c3a7e3f0477e008e5cc824425ab696b055
meankit025 <70406910+meankit025@users.noreply.github.com>
2024-07-05 23:06:03 +0530

    clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(function () {
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+  return <div>Hello React it's {time}</div>;
 }
 
 export default App;

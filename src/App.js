7ca2c665d17314838edce04f4ba31bdfa15f1d82
meankit025 <70406910+meankit025@users.noreply.github.com>
2024-07-05 18:06:48 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 const App = () => {
   const [advice, setAdvice] = useState("");
   const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     // Check if advice is already in LocalStorage
@@ -12,12 +13,19 @@ const App = () => {
     }
   }, []);
 
+  const clearDataHandler = () => {
+    localStorage.removeItem("myData");
+    setAdvice(null);
+    setCount(0);
+  };
+
   const clickHandler = async () => {
     setLoading(true);
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     const newAdvice = data.slip.advice;
     setAdvice(newAdvice);
+    setCount((c) => c + 1);
     localStorage.setItem("advice", newAdvice);
     setLoading(false);
   };
@@ -25,10 +33,23 @@ const App = () => {
   return (
     <div>
       <h1>{loading ? "Loading..." : advice}</h1>
+      <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+        <div>
+          <button onClick={clickHandler} disabled={loading}>
+            {loading ? "Fetching..." : "Get Advice"}
+          </button>
+        </div>
+        <div>
+          <button onClick={clearDataHandler} disabled={loading}>
+            Cleare Data
+          </button>
+        </div>
+      </div>
+
       <div>
-        <button onClick={clickHandler} disabled={loading}>
-          {loading ? "Fetching..." : "Get Advice"}
-        </button>
+        <p>
+          you have read <strong>{count}</strong> pieces of advice
+        </p>
       </div>
     </div>
   );
